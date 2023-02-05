ec3c56d3cb0909d4c35937b32af3193eca5b3838
fedegiorno <fedegiorno@gmail.com>
2023-02-05 11:33:02 -0300

    Al test de BT se le agrega un splash. Hay un problema con el icono del splash que no entra en el circulo, pero aparece el splash y pasa al ActivityMain

---
+++ app/src/main/java/com/fedegiorno/controlesdevuelo/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.fedegiorno.controlesdevuelo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Thread.sleep(1000)
+        screenSplash.setKeepOnScreenCondition { true }
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
